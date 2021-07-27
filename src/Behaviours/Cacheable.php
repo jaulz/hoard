15baa0a0ce6c76313954f30e52aa5e4ca9c032c2
Julian Hundeloh <julian.hundeloh+github@gmail.com>
2021-07-27 11:10:02 +0000

    fix: add cache method to Cacheable trait

--- src/Behaviours/Cacheable.php
+++ src/Behaviours/Cacheable.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace Jaulz\Eloquence\Behaviours;
 
+use Jaulz\Eloquence\Behaviours\Cacheable\Cache;
 use Jaulz\Eloquence\Behaviours\Cacheable\CacheObserver;
+use Jaulz\Eloquence\Commands\FindCacheableClasses;
+use ReflectionClass;
 
 trait Cacheable
 {
@@ -19,4 +23,47 @@ trait Cacheable
      * @return array
      */
     abstract public function caches();
+
+    /**
+     * Rebuild cache for the model.
+     *
+     * @return array
+     */
+    public function cache()
+    {
+        $className = get_class($this);
+
+        // Get all other model classes
+        $reflector = new ReflectionClass($className);
+        $directory = dirname($reflector->getFileName());
+        $classNames = (new FindCacheableClasses($directory))->getAllCacheableClasses();
+
+        // Go through all other classes and check if they reference the current class
+        $configs = collect([]);
+        collect($classNames)->each(function ($foreignClassName) use (
+            $className,
+            $configs
+        ) {
+            // Go through options and see where the model is referenced
+            $foreignConfigs = collect([]);
+            $foreignModel = new $foreignClassName();
+            collect($foreignModel->caches())
+                ->filter(function ($config) use ($className) {
+                    return $config['model'] === $className;
+                })
+                ->each(function ($config) use ($foreignConfigs) {
+                    $foreignConfigs->push($config);
+                });
+
+            // If there are no configurations that affect this model 
+            if ($foreignConfigs->count() === 0) {
+                return true;
+            }
+
+            $configs->put($foreignClassName, $foreignConfigs->toArray());
+        });
+
+        $cache = new Cache($this);
+        return $cache->rebuild($configs->toArray());
+    }
 }
