cf39663d0c037e0ce79930a3413ef3279aef5dbb
Julian Hundeloh <julian@hundeloh-consulting.ch>
2022-06-16 07:53:59 +0200

    fix: rename value in enum

--- src/Enums/HoardAggregationFunctionEnum.php
+++ src/Enums/HoardAggregationFunctionEnum.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Str;
 /**
  * @method static self sum()
  * @method static self count()
- * @method static self maximum()
- * @method static self minimum()
+ * @method static self max()
+ * @method static self min()
  * @method static self copy()
  * @method static self push()
  * @method static self group()
